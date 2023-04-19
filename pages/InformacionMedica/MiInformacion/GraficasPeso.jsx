483cba89a13af26727e6c8851fdc28a2612ab8d4
Ed Oz <orozcorp@gmail.com>
2023-04-19 13:14:01 -0600

    Update GraficasPeso.jsx

--- pages/InformacionMedica/MiInformacion/GraficasPeso.jsx
+++ pages/InformacionMedica/MiInformacion/GraficasPeso.jsx
@@ -61,7 +61,7 @@ export default function GraficasPeso({ userId }) {
     return <p>Cargando...</p>;
   }
   return (
-    <Box m={1} sx={{ width: ["100%", "40%"], height: ["100%", "50vh"] }}>
+    <Box m={1} sx={{ width: ["100%", "40%"], height: ["400px", "50vh"] }}>
       <Line
         data={data.createPesoGraphData}
         options={options}
