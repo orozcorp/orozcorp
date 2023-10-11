850df89d3fc3068e940370cda3408ba227e48725
Ed Oz <orozcorp@gmail.com>
2023-10-11 10:06:34 -0600

    updated

--- pages/Products/WhatsCRM/ProductFeatures.js
+++ pages/Products/WhatsCRM/ProductFeatures.js
@@ -8,35 +8,35 @@ export default function ProductFeatures() {
       description:
         "¿Ya tienes un CRM? Genial. ¿No tienes? También genial. WhatsCRM se adapta a ti, no al revés. Conéctate con cualquier CRM que tenga API y lleva tus ventas al siguiente nivel.",
       image:
-        "https://orozcorp.s3.us-east-2.amazonaws.com/orozcorp/orozcorp_a_phone_surrounded_by_icons_of_envelopes_minimalistic__cc70657c-5e76-4943-9046-549d1b8a031d.jpg",
+        "https://orozcorp.s3.us-east-2.amazonaws.com/orozcorp/orozcorp_2_galaxies_being_pulled_together_minmalistic_ying_yang_b90e10fa-c9e5-44a9-bb37-92df0648b24c.jpg",
     },
     {
       name: "Inventario al Alcance de tu Mano: Cierra Ventas en un Pestañeo",
       description:
         "¿Necesitas saber qué tienes en stock? ¡Boom! Ahí lo tienes. Con WhatsCRM, tu inventario está siempre a un clic de distancia. Cierra ventas al instante y deja a tus clientes boquiabiertos.",
       image:
-        "https://orozcorp.s3.us-east-2.amazonaws.com/orozcorp/orozcorp_a_dashboard_with_graphs_minimalistic__japanese_style_92be781d-6a1b-4014-a634-dcedf2317323.jpg",
+        "https://orozcorp.s3.us-east-2.amazonaws.com/orozcorp/orozcorp_a_well_organized_warehouse_with_clothes_minimalistic_8ef66d01-19e8-4616-a4e0-6c9f721f051b.jpg",
     },
     {
       name: "Comparte lo que Quieras: Más que Palabras",
       description:
         "¿Una imagen vale más que mil palabras? Entonces, ¿qué tal un video? Con WhatsCRM, envía documentos, imágenes y videos de hasta 50 MB. Convence, impresiona, cierra la venta.",
       image:
-        "https://orozcorp.s3.us-east-2.amazonaws.com/orozcorp/orozcorp_three_icons_of_users_surrounded_by_circles_that_overla_3d741583-c674-4634-8886-05ea14ee8a68.jpg",
+        "https://orozcorp.s3.us-east-2.amazonaws.com/orozcorp/orozcorp_a_phone_sharing_videos_and_photos_minimalistic_ab1bf041-d408-4e1b-ac95-f9f56038cf89.jpg",
     },
     {
       name: "Registro de Ventas: Tu Éxito, Nuestros Números",
       description:
         "¿Quieres saber cómo vas? Consulta todas tus ventas en un solo lugar. Y si ya tienes un ERP con API, lo conectamos para que no pierdas detalle.",
       image:
-        "https://orozcorp.s3.us-east-2.amazonaws.com/orozcorp/orozcorp_thousands_of_icons_of_users_viewing_their_phone_minima_7f61c608-9c23-4fce-bfe5-9b6163cc1328.jpg",
+        "https://orozcorp.s3.us-east-2.amazonaws.com/orozcorp/orozcorp_a_sales_dashboard_minimalistic_0cf05e9e-24df-411d-a002-b51b87bc43d4.jpg",
     },
     {
       name: "Seguridad de Primera: Tus Conversaciones son Sagradas",
       description:
         "¿Preocupado por la seguridad? Descansa tranquilo. Con el cifrado de extremo a extremo de WhatsCRM, tus conversaciones están más seguras que Fort Knox.",
       image:
-        "https://orozcorp.s3.us-east-2.amazonaws.com/orozcorp/orozcorp_a_clock_with_hands_grabing_a_cellphone_minimalistic_35599e59-5d3e-4801-ac12-53f10cdbe509.jpg",
+        "https://orozcorp.s3.us-east-2.amazonaws.com/orozcorp/orozcorp_end_to_end_encription_sms_messages_minmalistic_374c563d-d43e-4071-a9c6-6f5e0c6ba363.jpg",
     },
   ];
   return (
