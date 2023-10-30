514d5b9d43f74fa0328e122c0acf12f646c543cb
Ed Oz <orozcorp@gmail.com>
2023-10-30 10:54:10 -0600

    removed framer motion

--- components/landing/Sec2Box.js
+++ components/landing/Sec2Box.js
@@ -1,5 +1,5 @@
 import { Box, Heading, Text } from "@theme-ui/components";
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 export default function Sec2Box({ title, text }) {
   const { ref, inView } = useInView({
@@ -7,27 +7,25 @@ export default function Sec2Box({ title, text }) {
   });
 
   return (
-    <motion.button
-      initial={{ scale: 0 }}
-      animate={{ rotate: inView ? 360 : 0, scale: inView ? 1 : 0 }}
-      ref={ref}
-      whileHover={{ scale: 1.1 }}
-      whileTap={{ scale: 0.9 }}
-      transition={{
-        type: "spring",
-        stiffness: 260,
-        damping: 20,
-      }}
-      className="flex-1 w-[90%] lg:w-[380px]"
-    >
-      <Box m={4} p={[2, 4]} variant="styles.boxGlass">
-        <Heading mb={2} sx={{ color: "#000", fontSize: ["24px", "36px"] }}>
-          {title}
-        </Heading>
-        <Text sx={{ color: "#4a4a4a", fontSize: ["18px", "20px"] }}>
-          {text}
-        </Text>
-      </Box>
-    </motion.button>
+    // <motion.button
+    //   initial={{ scale: 0 }}
+    //   animate={{ rotate: inView ? 360 : 0, scale: inView ? 1 : 0 }}
+    //   ref={ref}
+    //   whileHover={{ scale: 1.1 }}
+    //   whileTap={{ scale: 0.9 }}
+    //   transition={{
+    //     type: "spring",
+    //     stiffness: 260,
+    //     damping: 20,
+    //   }}
+    //   className="flex-1 w-[90%] lg:w-[380px]"
+    // >
+    <Box m={4} p={[2, 4]} variant="styles.boxGlass">
+      <Heading mb={2} sx={{ color: "#000", fontSize: ["24px", "36px"] }}>
+        {title}
+      </Heading>
+      <Text sx={{ color: "#4a4a4a", fontSize: ["18px", "20px"] }}>{text}</Text>
+    </Box>
+    // </motion.button>
   );
 }
--- pages/Products/WhatsBlast/index.js
+++ pages/Products/WhatsBlast/index.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 import ProductFeatures from "./ProductFeatures";
 import ProductInformation from "./ProductInformation";
 import Contactanos from "../../../components/landing/Contactanos";
@@ -41,7 +41,7 @@ export default function WhatsBlast() {
         }}
       />
       <div className="flex flex-col md:flex-row flex-nowrap md:flex-wrap items-center justify-center gap-8 md:gap-20 mb-12 py-12">
-        <motion.button
+        {/* <motion.button
           initial={{ scale: 0, opacity: 0.3 }}
           animate={{ scale: 1, opacity: 1 }}
           whileHover={{ scale: 1.3 }}
@@ -51,16 +51,16 @@ export default function WhatsBlast() {
             stiffness: 260,
             damping: 20,
           }}
-        >
-          <Image
-            className="rounded-t-lg"
-            src="https://orozcorp.s3.us-east-2.amazonaws.com/orozcorp/orozcorp_envelopes.png"
-            alt="WhatsBlast: Tu Asistente de Mensajería Masiva"
-            width={300}
-            height={300}
-            priority
-          />
-        </motion.button>
+        > */}
+        <Image
+          className="rounded-t-lg"
+          src="https://orozcorp.s3.us-east-2.amazonaws.com/orozcorp/orozcorp_envelopes.png"
+          alt="WhatsBlast: Tu Asistente de Mensajería Masiva"
+          width={300}
+          height={300}
+          priority
+        />
+        {/* </motion.button> */}
         <div className="text-center md:text-left">
           <h1 className="text-3xl md:text-5xl font-bold">WhatsBlast</h1>
           <p className="text-xl md:text-3xl my-2 md:my-8">
