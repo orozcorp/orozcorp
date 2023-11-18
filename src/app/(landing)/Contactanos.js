5ddb983d7447031149b6bec0dc96603f8cfb079c
Ed Oz <orozcorp@gmail.com>
2023-11-17 19:10:20 -0600

    updated

--- src/app/(landing)/Contactanos.js
+++ src/app/(landing)/Contactanos.js
@@ -112,7 +112,7 @@ export default function Contactanos({ bgColor }) {
               disabled={loading}
               className={`text-zinc-100 bg-${
                 bgColor ? "white" : "black"
-              } border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2`}
+              } border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-10 py-3 me-2 mb-2`}
             >
               Enviar
             </button>
