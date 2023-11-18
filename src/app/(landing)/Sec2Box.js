1b31d05351d895ec690f91586c45c1ff0e33e2ed
Ed Oz <orozcorp@gmail.com>
2023-11-18 06:28:51 -0600

    updated

--- src/app/(landing)/Sec2Box.js
+++ src/app/(landing)/Sec2Box.js
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useState, useMemo, useCallback } from "react";
 import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 import useWindowSize from "../../components/hooks/useWindowSize";
@@ -16,8 +16,10 @@ export default function Sec2Box({
   });
   const [active, setActive] = useState(false);
   const size = useWindowSize();
-  const width = size.width;
-  const isMobile = width < 640;
+  const isMobile = useMemo(() => size.width < 640, [size.width]);
+  const toggleActive = useCallback(() => {
+    setActive((prevActive) => !prevActive);
+  }, []);
   return (
     <motion.button
       initial={{ scale: 0 }}
@@ -25,7 +27,7 @@ export default function Sec2Box({
       ref={ref}
       whileHover={{ scale: 1.1 }}
       whileTap={{ scale: 0.9 }}
-      onClick={() => setActive(!active)}
+      onClick={toggleActive}
       transition={{
         type: "spring",
         stiffness: 260,
@@ -36,17 +38,21 @@ export default function Sec2Box({
       <div
         className={`p-4 ${
           active ? "lg:p-0" : "lg:p-8"
-        } flex flex-row flex-wrap h-full  ${active && isMobile ? "items-center justify-center": "items-start justify-between"} `}
+        } flex flex-row flex-wrap h-full  ${
+          active && isMobile
+            ? "items-center justify-center"
+            : "items-start justify-between"
+        } `}
       >
         <div
           className={`flex flex-col flex-nowrap ${
-            active && "max-w-sm p-4 items-center justify-center w-full h-full"
+            active && "max-w-sm  items-center justify-center w-full h-full"
           }`}
         >
           <h3 className="mb-2 text-zinc-800 text-xl font-bold">{title}</h3>
           <div className="text-zinc-800 text-md text-center">{text}</div>
           {active && (
-            <div className="text-zinc-400 text-sm text-center my-8">
+            <div className="text-zinc-800 text-md text-center my-8">
               {textExpanded}
             </div>
           )}
