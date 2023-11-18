8c312e0681e07767bb9111363407342084988ee2
Ed Oz <orozcorp@gmail.com>
2023-11-17 19:31:16 -0600

    updated

--- src/app/(landing)/Section1.js
+++ src/app/(landing)/Section1.js
@@ -3,13 +3,10 @@ export default function Section1() {
   return (
     <div className="flex flex-col flex-nowrap bg-zinc-900 py-8">
       <div className="p-8 w-full">
-        <h2 className="my-8 text-4xl text-zinc-300 w-full text-center lg:text-left lg:max-w-4xl">
-          Te ayudamos a crear tu app, tu página, tu catálogo o tu idea. Logramos
-          que emprendas, o mejores tu sitio con la mayor facilidad e impacto.
+        <h2 className="my-8 text-4xl text-zinc-300 w-full text-center lg:text-left">
+          Creando apps, webs y catálogos digitales que impulsan tu negocio
+          <p>Entendiendo tu empresa paso a paso</p>
         </h2>
-        <div className="my-4 text-6xl lg:text-8xl text-white text-center lg:text-left">
-          Aumenta la eficiencia en cada paso.
-        </div>
       </div>
       <div className="p-8  text-zinc-100 text-center text-6xl">
         Nuestro Proceso
