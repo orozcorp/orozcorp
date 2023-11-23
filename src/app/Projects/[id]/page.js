97be80403ee343ee2ced71e168c105eae7183a3c
Ed Oz <orozcorp@gmail.com>
2023-11-22 18:58:32 -0600

    Update page.js

--- src/app/Projects/[id]/page.js
+++ src/app/Projects/[id]/page.js
@@ -75,6 +75,7 @@ export default async function Project({ params }) {
   const portfolio = portfolioQuery?.getPortfolioById;
   return (
     <>
+        <ScrollTop />
       <main className="flex flex-col flex-nowrap w-full justify-center items-center">
         <div className="flex flex-row flex-wrap p-4 w-full md:w-3/4 items-center justify-center">
           <div className="flex flex-col flex-nowrap w-full justify-start items-start">
