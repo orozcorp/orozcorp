92143926d8e3ec41919059501155e8db42325118
Ed Oz <orozcorp@gmail.com>
2023-11-26 13:16:27 -0600

    added analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,13 +20,20 @@ export default async function RootLayout({ children }) {
   return (
     <html lang="es">
       <link rel="icon" href="/favicon.ico" sizes="any" />
-      <Script id="google-analytics" strategy="lazyOnload" async>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload">
         {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-4SWYYHWMYH');
-        `}
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
       </Script>
       <body>
         <NextAuthSessionProvider>
