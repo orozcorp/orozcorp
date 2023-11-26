c0d02ed2967015e9c4bf230c7ed3d15070b35918
Ed Oz <orozcorp@gmail.com>
2023-11-26 07:28:08 -0600

    contacts

--- src/app/(landing)/Contactanos.js
+++ src/app/(landing)/Contactanos.js
@@ -113,7 +113,7 @@ export default function Contactanos({ bgColor }) {
               <textarea
                 id="message"
                 rows="4"
-                className="block p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 "
+                className="block p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border-b-2 border-zinc-300 focus:ring-blue-500 focus:border-blue-500 "
                 onChange={(e) =>
                   setValues({ ...values, message: e.currentTarget.value })
                 }
