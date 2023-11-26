2a6a4e6d3943212bef91c2952bc24b9c91b26db2
Ed Oz <orozcorp@gmail.com>
2023-11-26 16:59:34 -0600

    Update Articles.js

--- src/app/(landing)/Articles.js
+++ src/app/(landing)/Articles.js
@@ -15,8 +15,8 @@ export default async function Articles() {
   const blogs = data?.blogGetAll || [];
   return (
     <div className="bg-[#121212] text-white p-8">
-      <div className="flex flex-col flex-nowrap justify-center items-center gap-4">
-        <div className="text-3xl">Tips y lecturas para impulsar l </div>
+      <div className="flex flex-col flex-nowrap justify-center items-center gap-4 text-center">
+        <div className="text-3xl">Tips y lecturas para impulsar</div>
         <h2 className="text-6xl font-bold mb-8 text-center">tu Negocio</h2>
       </div>
       <div className="flex flex-col lg:flex-row flex-nowrap lg:flex-wrap gap-4 w-full my-8 justify-center lg:justify-between items-center lg:items-stretch">
