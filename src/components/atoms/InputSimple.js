1c1bf92e2033b2b13049694f9e7416816621e542
Ed Oz <orozcorp@gmail.com>
2023-11-29 16:04:39 -0600

    id shit

--- src/components/atoms/InputSimple.js
+++ src/components/atoms/InputSimple.js
@@ -10,7 +10,7 @@ export default function InputSimple({
     <div className="relative z-0  mb-5 group">
       <input
         type={type}
-        id={id}
+        id={label}
         name={label}
         className="block py-4 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
         required={required}
