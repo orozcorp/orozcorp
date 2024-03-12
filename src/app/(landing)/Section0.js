c9f0fbfd234c51da2226f17053e44449d0e15897
Orozcorp <orozcorp@gmail.com>
2024-03-12 17:38:08 -0600

    updated

--- src/app/(landing)/Section0.js
+++ src/app/(landing)/Section0.js
@@ -1,30 +1,16 @@
-"use client";
-import React, { memo } from "react";
 import Link from "next/link";
-import ReactRotatingText from "react-rotating-text";
-
-const RotatingText = memo(() => (
-  <ReactRotatingText
-    items={["Página", "ERP", "CRM", "Tienda"]}
-    color="rgb(185 28 28)"
-  />
-));
 
 export default function Section0() {
   return (
     <div className="flex flex-col flex-nowrap min-h-screen items-center justify-center p-4 gap-20">
-      <div className="flex flex-row flex-wrap justify-center items-basiline">
-        <div className="text-8xl text-center">De ideas a tu</div>
-        <div className="ml-4 text-8xl font-bold underline text-red-700">
-          <RotatingText />
-        </div>
-      </div>
       <div className="flex flex-row flex-wrap w-full lg:w-3/4 justify-center items-center">
-        <h1 className="text-zinc-800 text-center text-5xl">
+        <h1 className="text-zinc-800 text-center text-8xl">
           Tú lo imaginas, nosotros lo creamos
         </h1>
       </div>
-
+      <h2 className="text-center text-2xl">
+        Orozcorp: Tu Partner en Desarrollo Web y Aplicaciones Móviles
+      </h2>
       <button className="my-8 text-white bg-red-700 hover:bg-red-900 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-2xl px-5 py-2.5 me-2 mb-2">
         <Link href="#crearProductos" passHref>
           Averigua cómo
