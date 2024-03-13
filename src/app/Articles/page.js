edba46fa5c6ebe5b791d5565f9cf2d248d14082a
Orozcorp <orozcorp@gmail.com>
2024-03-13 08:09:19 -0600

    removed data

--- src/app/Articles/page.js
+++ src/app/Articles/page.js
@@ -1,65 +1,6 @@
 import Link from "next/link";
 import { format_date } from "../../lib/helpers/formatters";
 import { blogGetAll } from "../../server/articles";
-export async function generateMetadata({ params }) {
-  const id = params.id;
-  const portfolio = await blogGetAll({ id });
-  const keywords = [
-    "Orozcorp",
-    "Desarrollo Web",
-    "Next JS",
-    "GraphQL",
-    "JavaScript",
-    "Digital Solutions",
-    "Web Development Parnter",
-    "CRM",
-    "ERP",
-  ];
-  portfolio?.keywords?.map((val) => keywords.push(val));
-  return {
-    metadataBase: new URL(`https://orozcorp.live/Projects/${id}`),
-    title: `${portfolio?.project} - ${portfolio?.company} -  by Orozcorp`,
-    description: portfolio?.descriptionMeta,
-    image: portfolio?.images[0],
-    date: portfolio?.date,
-    type: "article",
-    url: `https://orozcorp.live/Projects/${id}`,
-    keywords: keywords,
-    twitter: {
-      card: "summary_large_image",
-      title: portfolio?.project,
-      creator: "@orozcorp_io",
-    },
-    robots: {
-      index: true,
-      follow: true,
-      nocache: true,
-      googleBot: {
-        index: true,
-        follow: true,
-        noimageindex: true,
-        "max-video-preview": -1,
-        "max-image-preview": "large",
-        "max-snippet": -1,
-      },
-    },
-    openGraph: {
-      title: portfolio?.project,
-      description: portfolio?.descriptionMeta,
-      image: portfolio?.images[0],
-      date: portfolio?.date,
-      type: "article",
-      url: `https://orozcorp.live/Projects/${id}`,
-      keywords: keywords,
-      article: {
-        publishedTime: portfolio?.date,
-        modifiedTime: portfolio?.date,
-        authors: ["https://www.orozcorp.io"],
-        tags: keywords,
-      },
-    },
-  };
-}
 export default async function Articles() {
   const data = await blogGetAll({ limit: 100 });
   return (
