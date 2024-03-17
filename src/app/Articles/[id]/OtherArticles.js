ba28215b1592c45d35d51bfa0ef0c22af8fc5dbd
Orozcorp <orozcorp@gmail.com>
2024-03-17 08:09:36 -0600

    updated

--- src/app/Articles/[id]/OtherArticles.js
+++ src/app/Articles/[id]/OtherArticles.js
@@ -2,13 +2,13 @@ import { blogGetAll } from "../../../server/articles";
 import Link from "next/link";
 
 export default async function OtherArticles() {
-  const { data: blogs } = await blogGetAll({ limit: 4 });
+  const blogs = await blogGetAll({ limit: 4 });
   return (
     <div className="w-full bg-black text-white mt-12 p-12 flex flex-col flex-nowrap">
       <div className="text-3xl text-center">Tips para mejorar tu </div>
       <h2 className="text-6xl font-bold mb-12 text-center">Negocio</h2>
       <div className="flex flex-col lg:flex-row flex-nowrap lg:flex-wrap gap-4 w-full mt-12 justify-center lg:justify-between items-center lg:items-stretch">
-        {blogs.map((blog) => (
+        {blogs?.map((blog) => (
           <Link
             href={`/Articles/${blog?._id}`}
             key={blog?._id}
