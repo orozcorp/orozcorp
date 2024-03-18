22440c90c3ecdd1ed4db87fe7377bd160c93b37b
Orozcorp <orozcorp@gmail.com>
2024-03-18 07:56:22 -0600

    fixed

--- src/app/Articles/[id]/page.js
+++ src/app/Articles/[id]/page.js
@@ -1,6 +1,6 @@
 import parse from "html-react-parser";
 import ScrollTop from "../../../components/atoms/ScrollTop";
-
+import Image from "next/image";
 import OtherArticles from "./OtherArticles";
 import Contactanos from "../../(landing)/Contactanos";
 import { blogGetById } from "../../../server/articles";
@@ -61,17 +61,30 @@ export default async function Article({ params }) {
           <div className="flex flex-col flex-nowrap w-full justify-start items-start "></div>
           <div className="flex flex-col flex-nowrap w-full justify-start items-start">
             <ScrollTop />
-            <div className="my-16 w-full break-words text-justify">
-              <h1 className="text-4xl font-bold my-6">{blog.title}</h1>
-              <div className="my-6 flex flex-row flex-wrap gap-2">
-                {blog?.article?.tags?.map((tag, index) => (
-                  <span
-                    key={index}
-                    className="bg-gray-100 text-gray-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded"
-                  >
-                    {tag}
-                  </span>
-                ))}
+            <div className="my-16 w-full break-words text-left md:text-justify ">
+              <div className="flex flex-row flex-wrap gap-4">
+                {blog?.images?.[0]?.url && (
+                  <Image
+                    src={blog?.images?.[0]?.url}
+                    alt={blog?.title}
+                    width={200}
+                    height={200}
+                    className="rounded-lg shadow-lg"
+                  />
+                )}
+                <div className="flex-1">
+                  <h1 className="text-4xl font-bold my-6">{blog.title}</h1>
+                  <div className="my-6 flex flex-row flex-wrap gap-2">
+                    {blog?.article?.tags?.map((tag, index) => (
+                      <span
+                        key={index}
+                        className="bg-gray-100 text-gray-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded"
+                      >
+                        {tag}
+                      </span>
+                    ))}
+                  </div>
+                </div>
               </div>
               {parse(blog?.content || "")}
             </div>
